869d139211bb6b7599d4819395b767b727ff9568
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-05-05 09:10:01 +0200

    add static files

--- src/main/java/com/sp/controller/RequestCrt.java
+++ src/main/java/com/sp/controller/RequestCrt.java
@@ -19,7 +19,7 @@
   	@Value("${welcome.message}")
   	private String message;
   
-  	private static String messageLocal="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+  	private static final String messageLocal="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
     private static List<AbstractCard> listCard = new ArrayList<AbstractCard>();
     static {
         listCard.add(new AbstractCard());
