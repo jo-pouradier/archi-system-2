f690a9b9c75836e724163270473293cbcfc21748
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-05-05 09:21:52 +0200

    merge

--- src/main/java/com/sp/controller/RequestCrt.java
+++ src/main/java/com/sp/controller/RequestCrt.java
@@ -22,8 +22,6 @@
   
   	private static final String messageLocal="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
     private static List<AbstractCard> listCard = new ArrayList<AbstractCard>();
-  	private static String messageLocal="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
-    private static List<Card> listCard = new ArrayList<Card>();
     static {
         listCard.add(new AbstractCard());
     }
