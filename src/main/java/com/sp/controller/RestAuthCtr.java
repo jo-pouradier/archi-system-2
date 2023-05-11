b26b9ffe189ace226770e53cd9257499b8b9cbc0
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-05-11 13:31:00 +0200

    auth

--- src/main/java/com/sp/controller/RestAuthCtr.java
+++ src/main/java/com/sp/controller/RestAuthCtr.java
@@ -1,29 +1,29 @@
 package com.sp.controller;
 
 import com.sp.service.AuthService;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class RestAuthCtr {
 
-    @GetMapping(value = {"/login"}, produces = "text/html")
-    public String login(Model model) {
-        String cookie = readCookie("user-id");
-        if(AuthService.logUser(cookie)){
-            model.addAttribute("message", "Welcome to the home page!");
-            model.addAttribute("messageLocal", "Welcome to the home page!");
-            return "html/login";
-        } else {
-            return "index";
-        }
-    }
+//        @PostMapping(value = "/login-form", produces = "text/html")
+//        public String login(@RequestParam("username") String username,
+//                            @RequestParam("password") String password,
+//                            Model model) {
+//            if (AuthService.login(username, password)) {
+//                return "redirect:/index";
+//            } else {
+//                return "redirect:/login";
+//            }
+//        }
 
-   // @GetMapping("/read-spring-cookie")
-    public String readCookie(
-            @CookieValue(name = "user-id", defaultValue = "0") String userId) {
-        return userId;
-    }
+
+
+
+
+        // @GetMapping("/write-spring-cookie")
+        // @GetMapping("/read-spring-cookie")
+
 
 }
