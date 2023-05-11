7f298458766484ddca11a318e2e4af0fe0f9d4ed
Ixion <hugues.farthouat@cpe.fr>
2023-05-11 17:30:27 +0200

    re

--- src/main/java/com/sp/controller/RestAuthCtr.java
+++ src/main/java/com/sp/controller/RestAuthCtr.java
@@ -1,6 +1,7 @@
 package com.sp.controller;
 
 import com.sp.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,22 +10,22 @@ import java.util.UUID;
 @RestController
 public class RestAuthCtr {
 
-        @PostMapping(value = "/login-form", produces = "text/html")
-        public UUID login(@RequestParam("username") String username, @RequestParam("password") String password) {
-             return AuthService.login(username, password); // on renvoie l'uuid ou null;
-        }
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping(value = "/login-form", produces = "text/html")
+    public UUID login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        return authService.login(username, password); // on renvoie l'uuid ou null;
+    }
 
     @PostMapping(value = "/register-form", produces = "text/html")
     public UUID register(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email) {
-        return AuthService.register(username, password, email); // on renvoie l'uuid ou null;
+        return authService.register(username, password, email); // on renvoie l'uuid ou null;
     }
 
 
-
-
-
-        // @GetMapping("/write-spring-cookie")
-        // @GetMapping("/read-spring-cookie")
+    // @GetMapping("/write-spring-cookie")
+    // @GetMapping("/read-spring-cookie")
 
 
 }
