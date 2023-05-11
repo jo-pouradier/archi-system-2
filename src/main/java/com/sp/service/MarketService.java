0703140bb7c40047ffdeca8a3d943ef349d9c344
Ixion <hugues.farthouat@cpe.fr>
2023-05-11 17:16:30 +0200

    blabla

--- src/main/java/com/sp/service/MarketService.java
+++ src/main/java/com/sp/service/MarketService.java
@@ -34,20 +34,4 @@ public class MarketService {
         return null;
     }
 
-//    private final UserService userService;
-//    private Card card;
-//    public MarketService(UserService userService) {
-//        this.userService = userService;
-//    }
-
-//    public boolean tradeCard(User from, User to, Card card){
-//        if (userService.getUser(from.getUUID()) && userService.getUser(to.getUUID())){
-//            if (from.getCardList().contains(card)){
-//                from.getCardList().remove(card);
-//                to.getCardList().add(card);
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
 }
