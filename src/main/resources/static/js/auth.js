26b290b41ac57c6a3e7e44c5b6ec6a97d9991c3f
Pierre-louis <pierre-louis.telep@cpe.fr>
2023-05-12 14:54:34 +0200

    if pas connecter rejeter

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -1,18 +1,27 @@
 function isUserLoggedIn() {
     // use cookie to check if user is logged in
     // if not, redirect to login page
-    let user = getCookie('user');
-    user = JSON.parse(user);
-    console.log(user);
-    if (user["uuid"] !== -1 || user !== undefined) {
-        console.log("coucou" + user["name"])
-        document.getElementById("userNameId").innerHTML = user["name"];
-        document.getElementById("Balance").innerHTML = user["balance"];
-        return true;
-    } else {
-        window.location.href = "/html/login.html";
-        return false;
+    console.log("isUserLoggedIn");
+
+    try {
+        let user = getCookie('user');
+        user = JSON.parse(user);
+        if (user["uuid"] !== -1 || user !== undefined) {
+            console.log("coucou" + user["name"])
+            document.getElementById("userNameId").innerHTML = user["name"];
+            document.getElementById("Balance").innerHTML = user["balance"];
+            return true;
+        } else {
+            window.location.href = "/html/login.html";
+            return false;
+        }
     }
+    catch (e) {
+        window.location.href = "/html/login.html";
+        console.log(e);
+    }
+
+
 }
 
 function getCookie(cname) {
